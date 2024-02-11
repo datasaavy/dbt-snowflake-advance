f83de45f5c8885c87336ee7b01fba39f95a39585
Vimal Kumar Vushakola <vimalvskl7@gmail.com>
2024-02-11 21:19:16 +0000

    marts_product_sales_analysis model created

--- models/Intemediate/int_order_items.sql
+++ models/Intemediate/int_order_items.sql
@@ -17,7 +17,7 @@ cln_order_items as (
             ,EXTRACT(MONTH FROM order_date) AS order_month_number
             ,TO_CHAR(order_date, 'Mon') AS order_month
             /*,LEFT(order_month_name, LEN(order_month_name) - 2) AS modified_value */
-            ,ROUND(SALE_PRICE, 2) as SALE_PRICE
+           /* ,ROUND(SALE_PRICE, 2) as SALE */
     FROM
     order_items
 )
